493b44377cd844d0518699463c4f8bf759c93657
d3rtb0y <sures.chaudhary0@gmail.com>
2019-12-18 13:41:57 +0545

    SMS sent Receiver SMS delivered Receiver

--- app/src/main/java/com/example/messaging/MainActivity.java
+++ app/src/main/java/com/example/messaging/MainActivity.java
@@ -3,10 +3,20 @@ package com.example.messaging;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+    String SENT = "SMS_SENT";
+    String DELIVERED = "SMS_DELIVERED";
+    PendingIntent sentPI, deliveredPI;
+    BroadcastReceiver smsSentReceiver, smsDeliveredReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +27,60 @@ public class MainActivity extends Activity {
         i.putExtra("sms_body","Greetings!");
         i.setType("vnd.android-dir/mms-sms");
         startActivity(i);
+
+        sentPI = PendingIntent.getBroadcast(this,0,new Intent(SENT),0);
+        deliveredPI = PendingIntent.getBroadcast(this, 0,new Intent(DELIVERED),0);
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+
+        //---create the BroadcastReceiver when the SMS is sent---
+        smsSentReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                switch (getResultCode()){
+                    case Activity.RESULT_OK:
+                        Toast.makeText(getBaseContext(),"SMS sent",Toast.LENGTH_SHORT).show();
+                        break;
+                        case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
+                            Toast.makeText(getBaseContext(),"Generic Failure",Toast.LENGTH_SHORT).show();
+                            break;
+
+                    case SmsManager
+                                .RESULT_ERROR_NO_SERVICE:
+                        Toast.makeText(getBaseContext(),"Generic failure",Toast.LENGTH_SHORT).show();
+                    break;
+                    case SmsManager.RESULT_ERROR_NULL_PDU:
+                        Toast.makeText(getBaseContext(),"NULL PDU",Toast.LENGTH_SHORT).show();
+                        break;
+                        case SmsManager.RESULT_ERROR_RADIO_OFF:
+                            Toast.makeText(getBaseContext(),"Radio off",Toast.LENGTH_SHORT).show();
+                            break;
+                }
+            }
+        };
+        //--- create the BroadcastReceiver when the SMS is delivered---
+        smsDeliveredReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context arg0, Intent arg1) {
+                switch (getResultCode()){
+                    case Activity.RESULT_OK:
+                        Toast.makeText(getBaseContext(),"SMS delivered",Toast.LENGTH_SHORT).show();
+                        break;
+                    case Activity.RESULT_CANCELED:
+                        Toast.makeText(getBaseContext(),"SMS not delivered",Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        };
+        //---register the two BroadcastReceiver---
+        registerReceiver(smsSentReceiver,new IntentFilter(SENT));
+        registerReceiver(smsDeliveredReceiver,new IntentFilter(DELIVERED));
+
+        //---send a SMS message---
+        SmsManager sms = SmsManager.getDefault();
+        sms.sendTextMessage("5556",null,"Greetings!",sentPI,deliveredPI);
     }
 }
