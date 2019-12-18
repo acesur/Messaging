538e912063a62c21b7ed93873c2a7f0384ac77c9
d3rtb0y <sures.chaudhary0@gmail.com>
2019-12-18 14:10:07 +0545

    Public Boolean SMS sent Receiver SMS delivered Receiver
    override on Pause

--- app/src/main/java/com/example/messaging/MainActivity.java
+++ app/src/main/java/com/example/messaging/MainActivity.java
@@ -5,14 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.database.ContentObservable;
+import android.database.ContentObserver;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.telephony.SmsManager;
+import android.util.Log;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
+    ContentResolver contentResolver;
+    ContentObserver smsContentObserver;
     String SENT = "SMS_SENT";
     String DELIVERED = "SMS_DELIVERED";
     PendingIntent sentPI, deliveredPI;
@@ -22,6 +31,10 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        contentResolver = getContentResolver();
+
+
+
         Intent i = new Intent(android.content.Intent.ACTION_VIEW);
         i.putExtra("address","5556; 5558; 5560");
         i.putExtra("sms_body","Greetings!");
@@ -61,6 +74,40 @@ public class MainActivity extends Activity {
                 }
             }
         };
+
+
+        @Override
+                public void onResume(){
+            super.onResume();
+            smsContentObserver = new ContentObserver( new Handler()) {
+            @Override
+                public void onChange(boolean selfChange){
+                Uri smsURI = Uri.parse("content://sms/sent");
+                Cursor c = getContentResolver().query(smsURI,new String[]{"address","date","body","type"},null,null,null);
+
+                String[] columns = new String[]{
+                        "address","date","body","type"
+                };
+
+                c.moveToNext();
+            String recipient = c.getString(c.getColumnIndex(columns[0]));
+            String date = c.getString(c.getColumnIndex(columns[1]));
+            String message = c.getString(c.getColumnIndex(columns[2]));
+            String type = c.getString(c.getColumnIndex(columns[3]));
+
+                Log.d("DetectOutgoingSMS",recipient + "," + date + ","+ message +","+type);
+            }
+
+            @Override
+                public boolean deliverSelfNotifications(){
+                return true;
+            }
+
+            };
+
+            contentResolver.registerContentObserver(Uri.parse("content://sms"),true,smsContentObserver);
+        }
+
         //--- create the BroadcastReceiver when the SMS is delivered---
         smsDeliveredReceiver = new BroadcastReceiver() {
             @Override
